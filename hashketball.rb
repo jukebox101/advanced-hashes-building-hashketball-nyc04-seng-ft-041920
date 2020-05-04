802f5548908528451bb892d127a7cec5669a597c
Leonora Squires <leonora.squires@gmail.com>
2020-05-04 18:46:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,17 @@ def game_hash
     :home => {
       :team_name => ""
       :color => []
-      :players => []
+      :players => [
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+        ]
     }
     :away => {
       :team_name =>
