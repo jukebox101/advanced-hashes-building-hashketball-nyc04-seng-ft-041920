34fbb87fcf202ea0291e84b07f41deac7d74bb45
Leonora Squires <leonora.squires@gmail.com>
2020-05-04 18:45:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,15 @@
 # Write your code here!
 def game_hash
-  
+  new_game_hash = {
+    :home => {
+      :team_name => ""
+      :color => []
+      :players => []
+    }
+    :away => {
+      :team_name =>
+      :color =>
+      :players =>
+    }
+  }
 end
