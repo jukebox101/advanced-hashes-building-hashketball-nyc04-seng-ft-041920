1f7801915fcbe698930bc9f286af66b1968932d4
Leonora Squires <leonora.squires@gmail.com>
2020-05-04 19:04:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,22 +18,6 @@ def game_hash
           }
         ]
     }
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :color => ["Turquoise", "Purple"],
-      :players => [
-        {
-          :player_name => "Jeff Adrien",
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
-          :slam_dunks => 2
-        }
-        ]
-    }
+    
   }
 end
