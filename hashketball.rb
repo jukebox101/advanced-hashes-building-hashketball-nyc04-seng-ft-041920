cd481473ef86036bb77e47cfa9168fc1b4896946
Leonora Squires <leonora.squires@gmail.com>
2020-05-04 19:10:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,6 +26,17 @@ def game_hash
           :steals => 12,
           :blocks => 12,
           :slam_dunks => 7
+          },
+          {
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
           }
         ]
     },
